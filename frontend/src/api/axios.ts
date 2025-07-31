42f0b6c251fa09ec569eb4942f3a43ca33f40930
Adéla Volfová <volfova.adela133@gmail.com>
2025-07-31 19:42:32 +0200

    Add headers automatically

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -7,4 +7,13 @@ const instance = axios.create({
     baseURL: 'http://localhost:8081/api',
 })
 
+// to avoid setting up the headers repeatedly => DRY
+instance.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default instance;
