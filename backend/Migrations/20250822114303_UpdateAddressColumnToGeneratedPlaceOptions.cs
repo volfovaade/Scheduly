f15c007ceeba4c2536e9edb1cb32e27bf5261d1f
Adéla Volfová <volfova.adela133@gmail.com>
2025-08-23 14:28:36 +0200

    Fix typo in db

---
+++ backend/Migrations/20250822114303_UpdateAddressColumnToGeneratedPlaceOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAddressColumnToGeneratedPlaceOptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Adress",
+                table: "GeneratedPlaceOptions",
+                newName: "Address");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "GeneratedPlaceOptions",
+                newName: "Adress");
+        }
+    }
+}
