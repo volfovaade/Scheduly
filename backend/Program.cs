78b6e0a61d4c9477768b06eec0f2b035b922c78d
Adéla Volfová <volfova.adela133@gmail.com>
2025-08-24 16:44:16 +0200

    Support to connect from other device

--- backend/Program.cs
+++ backend/Program.cs
@@ -31,9 +31,10 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "http://10.0.0.31:3000")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
