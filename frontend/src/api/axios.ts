d16e56441a7cee9f2bb69aef4f6f47937a1f520b
Adéla Volfová <volfova.adela133@gmail.com>
2025-08-24 16:44:48 +0200

    Change to connect from other device

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,7 +4,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8081/api',
+    baseURL: 'http://10.0.0.31:8081/api',  //'http://localhost:8081/api',
 })
 
 // to avoid setting up the headers repeatedly => DRY
